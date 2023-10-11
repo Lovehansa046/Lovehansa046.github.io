842ad6869389e2a847078bbf92a6881010a596ad
Lovehansa046 <107402926+Lovehansa046@users.noreply.github.com>
2023-10-11 13:04:33 +0300

    Add files via upload

---
+++ React_Project_2/precache-manifest.7761214f0bfa1bd817a9bdc9ce777ab4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "55a0e33c9a5b279313f8",
+    "url": "/React_Project_2/static/css/main.11b2d73a.chunk.css"
+  },
+  {
+    "revision": "55a0e33c9a5b279313f8",
+    "url": "/React_Project_2/static/js/main.55a0e33c.chunk.js"
+  },
+  {
+    "revision": "0002d18fba943baaa3e7",
+    "url": "/React_Project_2/static/css/1.33957fe6.chunk.css"
+  },
+  {
+    "revision": "0002d18fba943baaa3e7",
+    "url": "/React_Project_2/static/js/1.0002d18f.chunk.js"
+  },
+  {
+    "revision": "25b81a62afc5d6a6900f",
+    "url": "/React_Project_2/static/js/runtime~main.25b81a62.js"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/React_Project_2/static/media/error.22389cf4.jpg"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/React_Project_2/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "dd25a896889e83bbbbe0a6eb00489c6c",
+    "url": "/React_Project_2/index.html"
+  }
+];
