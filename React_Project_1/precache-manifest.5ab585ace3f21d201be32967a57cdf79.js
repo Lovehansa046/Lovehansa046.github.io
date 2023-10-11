d16b84b70403627968131f8985b7fe3263ba7f94
Lovehansa046 <107402926+Lovehansa046@users.noreply.github.com>
2023-10-11 13:06:58 +0300

    Add files via upload

---
+++ React_Project_1/precache-manifest.5ab585ace3f21d201be32967a57cdf79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "393455627983ede197e9",
+    "url": "/React_Project_1/static/css/main.49474f5b.chunk.css"
+  },
+  {
+    "revision": "393455627983ede197e9",
+    "url": "/React_Project_1/static/js/main.39345562.chunk.js"
+  },
+  {
+    "revision": "1d18980a45738a815c20",
+    "url": "/React_Project_1/static/css/1.c4e91802.chunk.css"
+  },
+  {
+    "revision": "1d18980a45738a815c20",
+    "url": "/React_Project_1/static/js/1.1d18980a.chunk.js"
+  },
+  {
+    "revision": "39e0a2b6fcbdd52cfab3",
+    "url": "/React_Project_1/static/js/runtime~main.39e0a2b6.js"
+  },
+  {
+    "revision": "4a72e9f3af4deadbf5f96bdd85f03ea0",
+    "url": "/React_Project_1/index.html"
+  }
+];
