a21380df162aeb2a2d870d2b2caaa0ebda486052
Lovehansa046 <107402926+Lovehansa046@users.noreply.github.com>
2023-10-11 12:57:22 +0300

    Add files via upload

---
+++ app.post/precache-manifest.d005677712efcdf27686a26f106ecf23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6e85a24b17b88a201e6",
+    "url": "/app.post/static/css/main.61c29ad2.chunk.css"
+  },
+  {
+    "revision": "b6e85a24b17b88a201e6",
+    "url": "/app.post/static/js/main.b6e85a24.chunk.js"
+  },
+  {
+    "revision": "a2b95fbb855fc3f7882b",
+    "url": "/app.post/static/css/1.74883b59.chunk.css"
+  },
+  {
+    "revision": "a2b95fbb855fc3f7882b",
+    "url": "/app.post/static/js/1.a2b95fbb.chunk.js"
+  },
+  {
+    "revision": "649a7ee9a6b1f5bada75",
+    "url": "/app.post/static/js/runtime~main.649a7ee9.js"
+  },
+  {
+    "revision": "92428a6672820ff3e075161440384c96",
+    "url": "/app.post/index.html"
+  }
+];
